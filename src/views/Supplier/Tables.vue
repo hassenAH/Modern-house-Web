64f60f2d3e579b715ce7a12468dba5d3ba584b92
Ahmedbachirmejri <ahmedbachir.mejri@esprit.tn>
2023-03-30 02:21:37 +0200

    carttable

--- src/views/Supplier/Tables.vue
+++ src/views/Supplier/Tables.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="flex flex-wrap mt-4">
     <div class="w-full mb-12 px-4">
-      <card-table />
+      <supplier-card-table />
     </div>
     <div class="w-full mb-12 px-4">
-      <card-table color="dark" />
+      <supplier-card-table color="dark" />
     </div>
   </div>
 </template>
 <script>
-import CardTable from "@/components/Cards/CardTable.vue";
+import SupplierCardTable from "@/components/Cards/SupplierCardTable.vue";
 
 export default {
   components: {
-    CardTable,
+    SupplierCardTable,
   },
 };
 </script>
